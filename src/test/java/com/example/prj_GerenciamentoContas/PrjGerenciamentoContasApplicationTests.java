f742c2d880255fa6fa11274e3f09a00a2b8a4419
jennifer cristine <jennifer.cristine11@gmail.com>
2024-03-23 11:10:28 -0300

    Add files via upload

---
+++ src/test/java/com/example/prj_GerenciamentoContas/PrjGerenciamentoContasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prj_GerenciamentoContas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjGerenciamentoContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
