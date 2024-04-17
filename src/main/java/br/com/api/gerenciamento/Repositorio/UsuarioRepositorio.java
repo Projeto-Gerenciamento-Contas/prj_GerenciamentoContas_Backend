98be653e0a7395f84bfbf405bc5bed8268888fb8
jennifer-cristine12 <jennifer.cristine11@gmail.com>
2024-04-17 17:04:02 -0300

    modelo endereço e Resposta usuario

--- src/main/java/br/com/api/gerenciamento/Repositorio/UsuarioRepositorio.java
+++ src/main/java/br/com/api/gerenciamento/Repositorio/UsuarioRepositorio.java
@@ -9,18 +9,18 @@ import org.springframework.stereotype.Repository;
 import br.com.api.gerenciamento.Modelo.UsuarioModelo;
 
 @Repository
-public interface GerenciamentoRepositorio extends CrudRepository<UsuarioModelo, Long> {
-	//listar todos os produtos
+public interface UsuarioRepositorio extends CrudRepository<UsuarioModelo, Long> {
+	//listar todos os usuarios
 	List<UsuarioModelo> findAll();
 	
 	//pesquisar por codigo
 	UsuarioModelo findByCodigo(int codigo);
 	
-	//remover produto
+	//remover usuario
 	
-	void delete(UsuarioModelo produto);
+	void delete(UsuarioModelo usuario);
 	
-	//Cadastrar/alterar produto
-	<ProdMod extends UsuarioModelo> ProdMod save(ProdMod produto);     
+	//Cadastrar/alterar usuario
+	<UsMod extends UsuarioModelo> UsMod save(UsMod usuario);     
 	
 }
